d2a11a2e81ac5e523e334e9784ea2191b554e36b
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-07-13 08:26:42 -0400

    added some donut support

--- esri2geo.js
+++ esri2geo.js
@@ -43,17 +43,53 @@ function poly(geometry){
     if(geometry.rings.length===1){
         return {"type": "Polygon","coordinates": geometry.rings};
     }else{
-        var r = [];
-        var l = geometry.rings.length;
-        var ii = 0;
-        while(l>ii){
-         r.push([geometry.rings[ii]]);
-         ii++;
-        }
-        return { "type": "MultiPolygon","coordinates":r}; 
+        var ccc= dP(geometry.rings);
+        var d = ccc[0];
+        var dd = ccc[1];
+        var r=[];
+        if(dd.length===0){
+            var l2 = d.length;
+            var i3 = 0;
+            while(l2>i3){
+             r.push([d[i3]]);   
+            }
+            return { "type": "MultiPolygon","coordinates":r}; 
+        }else if(d.length===1){
+            dd.unshift(d[0]);
+            return {"type": "Polygon","coordinates": dd};
+            
+        }else{
+            return { "type": "MultiPolygon","coordinates":d, "holes":dd};
+        }  
     }
 }
-    
-    
+function dP(a){
+    var d = [];
+        var dd =[];
+        var l = a.length;
+        var ii = 0;
+        while(l>ii){
+            if(c(a[ii])){
+                d.push(a[ii]);
+            }else{
+             dd.push(a[ii]);
+            }
+         ii++;
+        }
+    return [d,dd];
+}
+function c(a){
+ var l = a.length-1;
+ var i = 0;
+ var o=0;
+
+ while(l>i){
+ o+=(a[i][0]*a[i+1][1]-a[i+1][0]*a[i][1]);
+   
+     i++;
+ }
+    return o<=0;
+}   
 return outPut;  
 }
+
