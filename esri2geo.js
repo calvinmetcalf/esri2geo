41e5559810bdc93f00b35a4e975da25389589507
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-07-13 11:29:48 -0400

    added comments

--- esri2geo.js
+++ esri2geo.js
@@ -5,17 +5,22 @@ function toGeoJSON(data){
     var i = 0;
     while(fl>i){
      var ft = data.features[i];
+/* as only ESRI based products care if all the features are the same type of geometry, check for geometry type at a feature level*/
      var outFT = {
             "type": "Feature",
             "properties":ft.attributes
         };
         if(ft.geometry.x){
+//check if it's a point
           outFT.geometry=point(ft.geometry);
         }else if(ft.geometry.points){
+//check if it is a multipoint
             outFT.geometry=points(ft.geometry);
             }else if(ft.geometry.paths){
+//check if a line (or "ARC" in ESRI terms)
          outFT.geometry=line(ft.geometry);
         }else if(ft.geometry.rings){
+//check if a poly.
            outFT.geometry=poly(ft.geometry);  
         }
         
@@ -23,9 +28,11 @@ function toGeoJSON(data){
      i++;
     }
 function point(geometry){
+//this one is easy
     return {"type": "Point","coordinates": [geometry.x,geometry.y]};    
 }
 function points(geometry){
+//checks if the multipoint only has one point, if so exports as point instead
     if(geometry.points.length===1){
         return {"type": "Point","coordinates": geometry.points[0]};
     }else{
@@ -33,6 +40,7 @@ function points(geometry){
     }
 }
 function line(geometry){
+//checks if their are multiple paths or just one
     if(geometry.paths.length===1){
         return {"type": "LineString","coordinates": geometry.paths[0]};
     }else{
@@ -40,14 +48,17 @@ function line(geometry){
     }
 }
 function poly(geometry){
+//first we check for some easy cases, like if their is only one ring
     if(geometry.rings.length===1){
         return {"type": "Polygon","coordinates": geometry.rings};
     }else{
+/*if it isn't that easy then we have to start checking ring direction, basically the ring goes clockwise its part of the polygon, if it goes counterclockwise it is a hole in the polygon, but geojson does it by haveing an array with the first element be the polygons and the next elements being holes in it*/
         var ccc= dP(geometry.rings);
         var d = ccc[0];
         var dd = ccc[1];
         var r=[];
         if(dd.length===0){
+/*if their are no holes we don't need to worry about this, but do need to stuck each ring inside its own array*/
             var l2 = d.length;
             var i3 = 0;
             while(l2>i3){
@@ -55,15 +66,18 @@ function poly(geometry){
             }
             return { "type": "MultiPolygon","coordinates":r}; 
         }else if(d.length===1){
+/*if their is only one clockwise ring then we know all holes are in that poly*/
             dd.unshift(d[0]);
             return {"type": "Polygon","coordinates": dd};
             
         }else{
+/*if their are multiple rings and holes we have no way of knowing which belong to which without looking at it specially, so just dump the coordinates and add  a hole field, this may cause errors*/
             return { "type": "MultiPolygon","coordinates":d, "holes":dd};
         }  
     }
 }
 function dP(a){
+//returns an array of 2 arrays, the first being all the clockwise ones, the second counter clockwise
     var d = [];
         var dd =[];
         var l = a.length;
@@ -79,6 +93,7 @@ function dP(a){
     return [d,dd];
 }
 function c(a){
+//return true if clockwise
  var l = a.length-1;
  var i = 0;
  var o=0;
@@ -92,4 +107,3 @@ function c(a){
 }   
 return outPut;  
 }
-
